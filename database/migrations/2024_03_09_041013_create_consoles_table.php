01b9bfb2c4feff95831bb1b39a2d43a7e2791d55
Pacifista00 <adamsamudera99@gmail.com>
2024-03-09 12:55:49 +0700

    membuat migration dan relasi database

--- database/migrations/2024_03_09_041013_create_consoles_table.php
+++ database/migrations/2024_03_09_041013_create_consoles_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('consoles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('developer');
+            $table->string('release_year');
+            $table->integer('stock');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('consoles');
     }
 };
