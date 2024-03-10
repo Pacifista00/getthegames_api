b4b0e1bd1a5618e1703c46484436ccbebea5ecab
Pacifista00 <adamsamudera99@gmail.com>
2024-03-10 20:50:04 +0700

    membuat seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Genre;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,103 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $roles = [
+            [
+                "name" => "admin"
+            ],
+            [
+                "name" => "user"
+            ],
+        ];
+        $users = [
+            [
+                "username" => "admin",
+                "email" => "admin@mail.com",
+                "password" => bcrypt('admin123'),
+                "role_id" => 1,
+            ],
+            [
+                "username" => "user",
+                "email" => "user@mail.com",
+                "password" => bcrypt('user123'),
+                "role_id" => 2,
+            ],
+        ];
+        $genres = [
+            [
+                "name" => "Adventure"
+            ],
+            [
+                "name" => "Racing"
+            ],
+            [
+                "name" => "First Person Shooter"
+            ],
+            [
+                "name" => "Action"
+            ],
+            [
+                "name" => "Role-Playing-Games"
+            ],
+            [
+                "name" => "Puzzle"
+            ],
+            [
+                "name" => "Strategy"
+            ],
+            [
+                "name" => "Simulation"
+            ],
+            [
+                "name" => "Fighting"
+            ],
+            [
+                "name" => "Horror"
+            ],
+            [
+                "name" => "Music/Rhythm"
+            ],
+            [
+                "name" => "Sports"
+            ],
+            [
+                "name" => "Platformer"
+            ],
+            [
+                "name" => "Survival"
+            ],
+            [
+                "name" => "Visual Novel"
+            ],
+            [
+                "name" => "Sandbox"
+            ],
+            [
+                "name" => "Card & Board Games"
+            ],
+            [
+                "name" => "Education"
+            ]
+        ];
+        
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($roles as $role) {
+            Role::create([
+                'name' => $role['name']
+            ]);
+        }
+        foreach ($users as $user) {
+            User::create([
+                'username' => $user['username'],
+                'email' => $user['email'],
+                'password' => $user['password'],
+                'role_id' => $user['role_id'],
+            ]);
+        }
+        foreach ($genres as $genre) {
+            Genre::create([
+                'name' => $genre['name'],
+            ]);
+        }
     }
 }
